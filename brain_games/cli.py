d50fdc1c723999497be80d7f8bffcb9b129909cf
nik <grim322@bk.ru>
2021-04-19 19:46:34 +0300

    Committed for task 3

---
+++ brain_games/cli.py
@@ -0,0 +1,12 @@
+import prompt
+import brain_games
+
+
+def welcome_user():
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+
+
+if __name__ == "__main__":
+    welcome_user()
+
